60550488a906480aa170c24788172467ff1d9202
wyetuhdjk <gs1taik2009@hotmail.com>
2024-05-10 19:38:11 +0700

    init

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-function mergeTwoLists(l1, l2) {
-  const dummy = new ListNode();
-  let current = dummy;
-  while (l1 !== null && l2 !== null) {
-    if (l1.val < l2.val) {
-      current.next = l1;
-      l1 = l1.next;
-    } else {
-      current.next = l2;
-      l2 = l2.next;
+function canPartitionKSubsets(nums, k) {
+  const sum = nums.reduce((acc, val) => acc + val, 0);
+  if (sum % k !== 0) return false;
+  const target = sum / k;
+  nums.sort((a, b) => b - a);
+  if (nums[0] > target) return false;
+  const visited = new Array(nums.length).fill(false);
+  return backtrack(0, 0, k);
+  function backtrack(start, currentSum, groups) {
+    if (groups === 1) return true;
+    if (currentSum === target) return backtrack(0, 0, groups - 1);
+    for (let i = start; i < nums.length; i++) {
+      if (!visited[i] && currentSum + nums[i] <= target) {
+        visited[i] = true;
+        if (backtrack(i + 1, currentSum + nums[i], groups)) return true;
+        visited[i] = false;
+      }
     }
-    current = current.next;
+    return false;
   }
-  if (l1 !== null) {
-    current.next = l1;
-  }
-  if (l2 !== null) {
-    current.next = l2;
-  }
-  return dummy.next;
 }
